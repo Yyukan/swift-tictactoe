484c9f0e2b134d4806be3721781cbe92941db24a
Oleksandr Shtykhno <yukan@ukr.net>
2015-01-31 12:24:02 +0100

    Initial Commit

---
+++ TicTacToe/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TicTacToe
+//
+//  Created by Oleksandr Shtykhno on 31/01/15.
+//  Copyright (c) 2015 Oleksandr Shtykhno. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
